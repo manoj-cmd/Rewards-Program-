d87147571fb6b4279372664afd889131787354d2
Manoj <ansh@victoriousinfotech.com>
2024-07-02 22:26:33 +0530

    ceate a function line number 32 to 45  and call it

--- src/Component/RewardsComponent.js
+++ src/Component/RewardsComponent.js
@@ -26,23 +26,25 @@ const loadData = async (fetchTransactions, calculateRewards, setTransactions, se
   if (error) {
     return <div>{error}</div>;
   }
+   // Function to render customer rewards
+   const renderCustomerRewards = (customerId, rewards) => (
+    <div key={customerId}>
+      <h2>Customer {customerId}</h2>
+      <p>Total Points: {rewards[customerId].total}</p>
+      <b>Monthly Points:</b>
+      <ul>
+        {Object.keys(rewards[customerId].monthly).map((month) => (
+          <li key={month}>
+            Month {month}: {rewards[customerId].monthly[month]} points
+          </li>
+        ))}
+      </ul>
+    </div>
+   )
     return(
         <div className='mainDiv'>
         <h1>Rewards Program</h1>
-        {Object.keys(rewards).map((customerId) => (
-          <div key={customerId}>
-            <h2>Customer {customerId}</h2>
-            <p>Total Points: {rewards[customerId].total}</p>
-            <b>Monthly Points:</b>
-            <ul>
-              {Object.keys(rewards[customerId].monthly).map((month) => (
-                <li key={month}>
-                  Month {month}: {rewards[customerId].monthly[month]} points
-                </li>
-              ))}
-            </ul>
-          </div>
-        ))}
+          {Object.keys(rewards).map((customerId) => renderCustomerRewards(customerId, rewards))}
         </div>
     )
 }

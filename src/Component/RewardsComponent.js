deba9a1ec604620093c6310cf455beed5c892e03
Manoj <ansh@victoriousinfotech.com>
2024-07-15 21:13:53 +0530

    handled loading state while data loading

--- src/Component/RewardsComponent.js
+++ src/Component/RewardsComponent.js
@@ -3,31 +3,40 @@ import { fetchTransactions } from '../api';
 import { calculateRewards } from '../utils';
 import '../App.css'
 
-const RewardsComponete = () => {
+const RewardsComponent = () => {
   const [transactions, setTransactions] = useState([]);
   const [rewards, setRewards] = useState({});
   const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-// Pure function to handle loading data
-const loadData = async (fetchTransactions, calculateRewards, setTransactions, setRewards, setError) => {
-  try {
-    const data = await fetchTransactions();
-    setTransactions(data);
-    const calculatedRewards = calculateRewards(data);
-    setRewards(calculatedRewards);
-  } catch (error) {
-    setError('Failed to load transactions.');
-  }
-};
+  // Pure function to handle loading data
+  const loadData = async (fetchTransactions, calculateRewards, setTransactions, setRewards, setError, setLoading) => {
+    try {
+      const data = await fetchTransactions();
+      setTransactions(data);
+      const calculatedRewards = calculateRewards(data);
+      setRewards(calculatedRewards);
+    } catch (error) {
+      setError('Failed to load transactions.');
+    } finally {
+      setLoading(false);
+    }
+  };
+
   useEffect(() => {
-    loadData(fetchTransactions, calculateRewards, setTransactions, setRewards, setError);
+    loadData(fetchTransactions, calculateRewards, setTransactions, setRewards, setError, setLoading);
   }, []);
+
+  if (loading) {
+    return <h3>Loading...</h3>;
+  }
 
   if (error) {
     return <div>{error}</div>;
   }
-   // Function to render customer rewards
-   const renderCustomerRewards = (customerId, rewards) => (
+
+  // Function to render customer rewards
+  const renderCustomerRewards = (customerId, rewards) => (
     <div key={customerId}>
       <h2>Customer {customerId}</h2>
       <p>Total Points: {rewards[customerId].total}</p>
@@ -40,13 +49,14 @@ const loadData = async (fetchTransactions, calculateRewards, setTransactions, se
         ))}
       </ul>
     </div>
-   )
-    return(
-        <div className='mainDiv'>
-        <h1>Rewards Program</h1>
-          {Object.keys(rewards).map((customerId) => renderCustomerRewards(customerId, rewards))}
-        </div>
-    )
-}
-export default RewardsComponete
+  );
 
+  return (
+    <div className='mainDiv'>
+      <h1>Rewards Program</h1>
+      {Object.keys(rewards).map((customerId) => renderCustomerRewards(customerId, rewards))}
+    </div>
+  );
+};
+
+export default RewardsComponent;
